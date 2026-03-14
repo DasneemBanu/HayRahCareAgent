e91cb5d672d2abd9d160b7b16186cb0dc7f8370b
DASNEEM BANU BINTI HAJA <dasneembanu05@gmail.com>
2026-03-15 03:40:07 +0800

    Update summarizer_agent.py

--- agents/summarizer_agent.py
+++ agents/summarizer_agent.py
@@ -1,14 +1,16 @@
+
+
 import streamlit as st
 import google.generativeai as genai
 
-api_key =st.secrets["GEMINI_API_KEY"]
-
+api_key = st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 def summarize_research(question, context):
-
+    # truncate context just in case
+    context = context[:4000]
     prompt = f"""
 You are HayRahCare, a mental health research assistant.
 
@@ -21,7 +23,5 @@ Question:
 
 Provide a clear evidence-based summary.
 """
-
     response = model.generate_content(prompt)
-
     return response.text
